c617744fc5f14977e74bbaebc78749a48c1d9b0b
khandu443 <ravisagar328@gmail.com>
2016-03-08 14:01:35 +0530

    phpinfo

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>My first PHP page</h1>
-
 <?php
-echo "Hello World!";
-?>  
 
-</body>
-</html>
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+?>
